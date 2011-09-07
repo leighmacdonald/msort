4ecab4be81967bee38a8306a3171097ff38fa618
Leigh MacDonald <leigh.macdonald@gmail.com>
2011-09-07 10:08:58 -0600

    Updated setup script to support running test cases

--- setup.py
+++ setup.py
@@ -2,9 +2,43 @@
 """
 Author: Leigh MacDonald <leigh.macdonald@gmail.com>
 """
+from unittest import TextTestRunner, TestLoader
+from glob import glob
+from os.path import splitext, basename, join as pjoin
+from os import getcwd
+try:
+    from os.path import walk
+except ImportError:
+    from os import walk
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup, Command
 
+class TestCommand(Command):
+    user_options = [ ]
+
+    def initialize_options(self):
+        self._dir = getcwd()
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        """
+        Finds all the tests modules in tests/, and runs them.
+        """
+        testfiles = [ ]
+        for t in glob(pjoin(self._dir, 'tests', '*.py')):
+            if not t.endswith('__init__.py'):
+                testfiles.append('.'.join(
+                    ['tests', splitext(basename(t))[0]])
+                )
+
+        tests = TestLoader().loadTestsFromNames(testfiles)
+        t = TextTestRunner(verbosity = 1)
+        t.run(tests)
 setup(
     name='msort',
     version='1.0',
@@ -14,6 +48,7 @@ setup(
     url='https://msort.cudd.li/',
     packages=['msort'],
     scripts=['scripts/sort.py'],
+    cmdclass = { 'test': TestCommand },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
