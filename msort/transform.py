1a278c9919d831071eeb328ae8d112c5dba7be39
leigh@cudd.li <leigh.macdonald@gmail.com>
2012-06-27 11:31:13 -0600

    Added the transform.splitucwords function

--- msort/transform.py
+++ msort/transform.py
@@ -13,5 +13,27 @@ def cleanup(word, sep='.'):
     w = re.sub('\{0}+'.format(sep), sep, sep.join(sep.join(word.split()).replace('-', sep).split(sep)))
     if w.endswith(sep):
         w = w[0:len(w)-1]
-
     return upperwords(w)
+
+def splitucwords(words):
+    size = len(words)-1
+    new_words = []
+    tmp_word = ''
+    for i, c in enumerate(words):
+        if i == 0 or c.isupper():
+            tmp_word += c
+        else:
+            if i+1 <= size:
+                if words[i+1].isupper():
+                    # Next character is uppercase, so this is the end of the current word
+                    tmp_word += c
+                    new_words.append(tmp_word)
+                    tmp_word = ''
+                else:
+                    tmp_word += c
+            else:
+                tmp_word += c
+                new_words.append(tmp_word)
+    return new_words
+
+
