9fa89edacdd8ce6e5ea4db6b3052fa1a11b0e615
Leigh MacDonald <leigh@cudd.li>
2012-06-30 13:02:35 -0600

    Added the operation manager to help facilitate with executing the queued operations.

--- mediasort.py
+++ mediasort.py
@@ -19,7 +19,7 @@ from optparse import OptionParser
 from msort.conf import Config, ConfigError
 from msort.log import getLogger
 from msort.filesystem import DirectoryScanner
-from msort.operation import OperationError
+from msort.operation import OperationError, OperationManager
 from msort.check.empty import EmptyCheck
 from msort.check.release import ReleaseCheck
 from msort.check.inuse import InUseCheck
@@ -29,13 +29,7 @@ try:     get_input = raw_input
 except:  get_input = input
 confirm = lambda m: get_input('{0} [Y/n]: '.format(m)).lower() in ('y', '')
 
-def main():
-    """ Parse command line arguments and run the sorter
-
-    :return: Program exit code
-    :rtype: int
-    """
-    # Parse CLI options
+def parse_cli(args=None):
     parser = OptionParser(version='2.0')
     parser.add_option('-d', '--debug', dest="debug", action="store_true", default=False,
         help="Enable debugging output level")
@@ -43,12 +37,21 @@ def main():
         help="Answer yes for all question, autocommit changes found")
     parser.add_option('-c', '--config', dest="config_file", default="~/.msort.conf", metavar='CONFIG_FILE',
         help="Set an alternate config file path")
-    options, args = parser.parse_args()
+    return parser.parse_args(args)
+
+def main():
+    """ Parse command line arguments and run the sorter
+
+    :return: Program exit code
+    :rtype: int
+    """
+    # Parse CLI options
+    options, args = parse_cli()
 
     # Setup Logger
     log = getLogger(__name__)
     log_level = DEBUG if options.debug else INFO
-    basicConfig(level=log_level, format='%(message)s')
+    #basicConfig(level=log_level, format='%(message)s')
     ret_code = 0
     cur_idx = 0
     errors_list = []
@@ -61,29 +64,15 @@ def main():
         scanner.registerChecker(InUseCheck(conf))
         scanner.registerChecker(EmptyCheck(conf))
         scanner.registerChecker(ReleaseCheck(conf))
-        changes = {}
+        operation_mgr = OperationManager(conf.getboolean('general','error_continue'))
         for section in conf.filteredSections():
-            changes[section] = scanner.find(section)
-        total_changes = sum([len(x) for x in changes.values()])
-        log.info('Found {0} total changes to be executed'.format(total_changes))
-        if options.autocommit or confirm('Apply changes found ({0})?'.format(total_changes)):
-
-            for section, operations in changes.items():
-                for oper in operations:
-                    cur_idx +=1
-                    log.info('[{1}/{2}] {0}'.format(str(oper), cur_idx, total_changes))
-                    try:
-                        oper()
-                    except OperationError as err:
-                        if not conf.getboolean('general', 'error_continue'):
-                            raise
-                        errors_list.append(err)
-            if errors_list:
-                log.error('There were {0} errors encountered while executing all operations:'.format(len(errors_list)))
-                for i, error in enumerate(errors_list):
-                    log.error('[{0}] {1}'.format(i, error))
+            operation_mgr[section] = scanner.find(section)
+        log.info('Found {0} total changes to be executed'.format(len(operation_mgr)))
+        if options.autocommit or confirm('Apply changes found ({0})?'.format(len(operation_mgr))):
+            if operation_mgr.execute():
+                log.info('Completed all operations successfully! [{0}/{1}]'.format(cur_idx, len(operation_mgr)))
             else:
-                log.info('Completed all operations successfully! [{0}/{1}]'.format(cur_idx, total_changes))
+                operation_mgr.showErrors()
     except ConfigError as err:
         log.error('There was a configuration error:\n{0}'.format(err))
         ret_code = 3
@@ -96,10 +85,10 @@ def main():
         ret_code = 1
     else:
         log.info('With the speed of an African Swallow')
-        if total_changes and cur_idx == total_changes and not errors_list:
-            log.info('All {0} operations completed successfully!'.format(total_changes))
+        if len(operation_mgr) and cur_idx == len(operation_mgr) and not errors_list:
+            log.info('All {0} operations completed successfully!'.format(len(operation_mgr)))
         else:
-            if errors_list:
+            if operation_mgr.error_list:
                 log.info('Errors were encountered, you should review them and make any changes deemed required.')
             else:
                 log.info('No operations to perform were found.')
--- msort/operation.py
+++ msort/operation.py
@@ -7,9 +7,12 @@ from msort.log import getLogger
 from msort.filesystem import dir_size, fmt_size
 
 class OperationError(MSortError):
+    """ Thrown on a error during a operantion """
     pass
 
 class BaseOperation(object):
+    """ Sets up the logger instance for the operation """
+
     def __init__(self):
         self.log = getLogger(__name__)
 
@@ -20,6 +23,8 @@ class BaseOperation(object):
         return self.__class__.__name__
 
 class MoveOperation(BaseOperation):
+    """ Used to move files from one directory to another"""
+
     def __init__(self, source, destination):
         BaseOperation.__init__(self)
         self.source = source
@@ -37,12 +42,22 @@ class MoveOperation(BaseOperation):
         return '{0} {1} {2}'.format(self.__class__.__name__, self.source, self.destination)
 
 class DeleteOperation(BaseOperation):
+    """ Used to delete a path from the filesystem """
+
     def __init__(self, path):
+        """ Set the path to be deleted
+
+        :param path: Path to delete
+        :type path: str
+        """
         BaseOperation.__init__(self)
         self.source = path
         self.size = None
 
     def __call__(self):
+        """ Perform the delete operation taking account for a directory or a folder
+        being deleted
+        """
         if isdir(self.source):
             rmtree(self.source)
             self.log.info('Removed directory: {0}'.format(self.source))
@@ -51,6 +66,40 @@ class DeleteOperation(BaseOperation):
             self.log.info('Removed file: {0}'.format(self.source))
 
     def __str__(self):
-        if not self.size:
+        if self.size == None:
             self.size = dir_size(self.source)
         return '{0} ({1}) {2}'.format(fmt_size(self.size), self.__class__.__name__, self.source)
+
+class OperationManager(dict):
+    """
+    Oversees executing queued up operations.
+    """
+    def __init__(self, error_continue=False):
+        dict.__init__(self)
+        self.log = getLogger(__name__)
+        self.log.propagate = 1
+        self.error_list = []
+        self.error_continue = error_continue
+        self.cur_idx = 0
+
+    def executeSection(self, section):
+        for oper in self[section]:
+            self.cur_idx +=1
+            self.log.info('[{1}/{2}] {0}'.format(str(oper), self.cur_idx, len(self)))
+            try: oper()
+            except OperationError as err:
+                if not self.error_continue:
+                    raise
+                self.error_list.append(err)
+        return self.error_list
+
+    def execute(self, sections=None):
+        return not any(map(self.executeSection, sections if sections else self.keys()))
+
+    def showErrors(self):
+        self.log.error('There were {0} errors encountered while executing all operations:'.format(len(self.error_list)))
+        for i, error in enumerate(self.error_list):
+            self.log.error('[{0}] {1}'.format(i, error))
+
+    def __len__(self):
+        return sum([len(x) for x in self.values()])
